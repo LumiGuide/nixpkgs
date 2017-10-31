e0468da1aad461ed1751cb1e399ad4758da424a4
Bas van Dijk <v.dijk.bas@gmail.com>
2017-10-31 15:57:33 +0100

    wordpress: 4.7.4 -> 4.8.2

--- pkgs/servers/web-apps/wordpress/default.nix
+++ pkgs/servers/web-apps/wordpress/default.nix
@@ -2,11 +2,12 @@
 { fetchFromGitHub, lib } : fetchFromGitHub {
   owner = "WordPress";
   repo = "WordPress";
-  rev = "4.7.4";
-  sha256 = "1ia9d3n085x2r6pvdrv4rk6gdp6xhjhpsq5g7a6448xzv9hf14ri";
+  rev = "4.8.2";
+  sha256 = "1bgj5zj50vcph0m8h6gzc7gphnp6qkfz5fgcrinji08dbbmqhfr3";
   meta = {
     homepage = https://wordpress.org;
     description = "WordPress is open source software you can use to create a beautiful website, blog, or app.";
     license = lib.licenses.gpl2;
+    maintainers = [ lib.maintainers.basvandijk ];
   };
 }
