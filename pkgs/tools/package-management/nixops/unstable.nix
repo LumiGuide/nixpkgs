97f451909a831c21da3d44e1aadc82eacef88117
Bas van Dijk <v.dijk.bas@gmail.com>
2017-11-20 18:16:31 +0100

    nixopsUnstable: don't depend on external URLs
    Instead of downloading a tarball from https://static.domenkozar.com we
    build the tarball ourself by downloading nixops's source code from
    GitHub and importing the release.nix file.

--- pkgs/tools/package-management/nixops/unstable.nix
+++ pkgs/tools/package-management/nixops/unstable.nix
@@ -1,10 +1,39 @@
-{ callPackage, fetchurl }:
+{ callPackage, lib, path, fetchFromGitHub }:
 
-callPackage ./generic.nix (rec {
-  version = "2017-05-22";
-  src = fetchurl {
-    # Sadly hydra doesn't offer download links
-    url = "https://static.domenkozar.com/nixops-1.5.1pre2169_8f4a67c.tar.bz2";
-    sha256 = "0rma5npgkhlknmvm8z0ps54dsr07za1f32p6d6na3nis784h0slw";
+let
+  rev = "af5d021ddbb8817922f572127a67907261ff362b";
+
+  src = fetchFromGitHub {
+    owner = "LumiGuide";
+    repo  = "nixops";
+    inherit rev;
+    sha256 = "0bzmb13czw4hm5k16085jl6ch3fhlxnnqv8z0c0n5p9gn5bf6gg4";
   };
-})
+
+  nixopsSrc = {
+    outPath = src;
+    revCount = 0;
+    shortRev = lib.substring 0 6 rev;
+    inherit rev;
+  };
+
+  officialRelease = false;
+
+  release = import (src + "/release.nix") {
+    inherit nixopsSrc;
+    inherit officialRelease;
+    nixpkgs = path;
+  };
+
+  version = "1.6" + (
+    if officialRelease
+    then ""
+    else "pre${toString nixopsSrc.revCount}_${nixopsSrc.shortRev}"
+  );
+
+  releaseName = "nixops-${version}";
+
+in callPackage ./generic.nix {
+  inherit version;
+  src = release.tarball + "/tarballs/${releaseName}.tar.bz2";
+}
