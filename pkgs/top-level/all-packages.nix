d6b0c3c8486d2810954f54d3eb2256c0ec48c78a
Lars Jellema <lars.jellema@gmail.com>
2022-06-10 11:56:35 +0200

    Revert removal of electron_{7,8}
    This partially reverts commit cdfac76b72a4e7cd199923d63f6bce81407ff179.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17725,6 +17725,8 @@ with pkgs;
 
   inherit (callPackages ../development/tools/electron/binary { })
     electron-bin
+    electron_7-bin
+    electron_8-bin
     electron_9-bin
     electron_10-bin
     electron_11-bin
@@ -17744,6 +17746,8 @@ with pkgs;
     electron_25-bin;
 
   electron = electron-bin;
+  electron_7 = electron_7-bin;
+  electron_8 = electron_8-bin;
   electron_9 = electron_9-bin;
   electron_10 = electron_10-bin;
   electron_11 = electron_11-bin;
