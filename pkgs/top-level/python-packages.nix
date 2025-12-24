e4afb829e1f8e621298754be3cbeddd50ca18565
Gaetan Lepage <gaetan@glepage.com>
2025-12-24 00:29:48 +0000

    python3Packages.onnx-ir: init at 0.1.14_1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11069,6 +11069,8 @@ self: super: with self; {
 
   onnx = callPackage ../development/python-modules/onnx { };
 
+  onnx-ir = callPackage ../development/python-modules/onnx-ir { };
+
   onnxconverter-common = callPackage ../development/python-modules/onnxconverter-common { };
 
   onnxmltools = callPackage ../development/python-modules/onnxmltools { };
